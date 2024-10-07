badc6d42aa6ae503c7aed243d77aaed1617bdd0f
VladislavYV <vladinator.d@gmail.com>
2024-10-07 09:25:34 +0500

    control

--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -6,15 +6,47 @@ export function Set() {
     const cards = require('../data.json');
     const [step, setStep] = useState(0);
 
+    const handleNext = () => {
+        if (step < cards.length - 1) {
+            setStep(step + 1)
+        }
+    }
+
+    const handlePrev = () => {
+        if (step > 0) {
+            setStep(step - 1)
+        }
+    }
 
     return (
         <div>
-            <h2>Название набора</h2>
+            <h2 className="title-cards">Название набора</h2>
+
             <Card
-                front = { cards[step].front }
-                back = { cards[step].back }
+                key={cards[step].id}
+                front={cards[step].front}
+                back={cards[step].back}
             />
-            <div>Управление</div>
+
+            <div className="control-cards">
+                <button
+                    className="control-btn"
+                    onClick={handlePrev}
+                    disabled={step == 0 ? true : false}
+                >
+                    «
+                </button>
+                <p>
+                    {step + 1} / {cards.length}
+                </p>
+                <button
+                    className="control-btn"
+                    onClick={handleNext}
+                    disabled={step == cards.length - 1 ? true : false}
+                >
+                    »
+                </button>
+            </div>
         </div>
     );
 }
