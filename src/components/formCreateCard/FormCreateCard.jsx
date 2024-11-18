2b67df388ad86d508132d587c83d11614305e1ff
VladislavYV <vladinator.d@gmail.com>
2024-11-18 10:33:03 +0500

    validation

--- src/components/formCreateCard/FormCreateCard.jsx
+++ src/components/formCreateCard/FormCreateCard.jsx
@@ -1,13 +1,61 @@
+import './FormCreateCard.css';
+import { useForm } from "react-hook-form";
+
 export function FormCreateCard() {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            front: '',
+            back: ''
+        }
+    });
+
     return (
-        <form action="">
-            <label>
-                Передняя сторона карточки: <input type="text" name="fronttext" />
-            </label>
-            <label>
-                Передняя сторона карточки: <input type="text" name="backtext" />
-            </label>
-            <input type="submit" value="Создать карточку"/>
+        <form className='create-card__form' onSubmit={handleSubmit((data) => {
+            console.log(data);
+        })}>
+            <div className='form__item'>
+                <label className='create-card__front' htmlFor='front'>
+                    Передняя сторона карточки:
+                </label>
+                <input 
+                    type="text" 
+                    name="front"
+                    id='front'
+                    {...register("front", {
+                        required: "Напишите текст для передней стороны карточки.",
+                        minLength: {
+                            value: 3,
+                            message: "Кол-во символов должно быть больше 3."
+                        }
+                    })} 
+                />
+                <p className='error-msg'>{errors.front?.message}</p>
+            </div>
+            <div className='form__item'>
+                <label className='create-card__back' htmlFor='back'>
+                    Задняя сторона карточки:
+                </label>
+                <input 
+                    type="text" 
+                    name="back" 
+                    id='front'
+                    {...register("back", {
+                        required: "Напишите текст для задней стороны карточки.",
+                        minLength: {
+                            value: 3,
+                            message: "Кол-во символов должно быть больше 3."
+                        }
+                    })} 
+                />
+                <p className='error-msg'>{errors.back?.message}</p>
+            </div>
+            <div className='form__item'>
+                <input
+                    className="create-card__btn"
+                    type="submit" 
+                    value="Создать карточку" 
+                />
+            </div>
         </form>
     )
 }
--- src/components/formCreateSet/FormCreateSet.jsx
+++ src/components/formCreateSet/FormCreateSet.jsx
@@ -1,13 +1,77 @@
+import './FormCreateSet.css'
+// import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export function FormCreateSet() {
+    const { register, handleSubmit, formState : {errors} } = useForm({
+        defaultValues: {
+            name:'',
+            description: ''
+        }
+    });
+    console.log(errors);
+
+    // const [name, setName] = useState('');
+    // const [description, setDesc] = useState('');
+
+    // function handleNameChange(e) {
+    //     setName(e.target.value);
+    // }
+
+    // function handleDescChange(e) {
+    //     setDesc(e.target.value);
+    // }
+
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     if(name == '' || description == ''){
+    //         alert("Одно из полей не заполнено.")
+    //     } else {
+    //         console.log(name,description);
+    //     }
+    // }
+
     return (
-        <form>
-            <label>
-                Название набора: <input type="text" name="name" />
-            </label>
-            <label>
-                Описание: <textarea name="discrition"/>
-            </label>
-            <input type="submit" value="Создать сет" />
+        <form className="create-set__form" onSubmit={handleSubmit((data) => {
+            console.log(data);
+        })}>
+            <div className="form__item">
+                <label className="create-set__name" htmlFor="name">
+                    Название набора:
+                </label>
+                <input
+                    type="text"
+                    name="name"
+                    id='name'
+                    {...register("name", { 
+                        required: "Напишите название набора.", 
+                        minLength: {
+                            value: 3,
+                            message: "Название набора должно состоять минимум из 3 символов."
+                        }
+                    })}
+                />
+                <p className='error-msg'>{errors.name?.message}</p>
+            </div>
+            <div className='form__item'>
+                <label className="create-set__desc" htmlFor="description">
+                    Описание:
+                </label>
+                <textarea
+                    name="discrition"
+                    id='description'
+                    rows={6}
+                    {...register("description", { required: "Заполните описание набора." })}
+                />
+                <p className='error-msg'>{errors.description?.message}</p>
+            </div>
+            <div className='form__item'>
+                <input
+                    className="create-set__btn"
+                    type="submit"
+                    value="Создать сет"
+                />
+            </div>
         </form>
     );
 }
